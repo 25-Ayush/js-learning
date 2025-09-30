75cefa2f43d9da565c03d6bb0a6c72539697a90f
25-Ayush <155434945+25-Ayush@users.noreply.github.com>
2025-09-30 16:00:43 +0000

    Conversions and Operations

---
+++ 01_Basics/03_conversionsOperations.js
@@ -0,0 +1,28 @@
+let score = "25ab";
+console.log(typeof score); //string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); //true
+
+// 1 => true ; 0 => false
+// "" =>false
+// "ayush" => true
+
+
+let someNumber = 25;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
