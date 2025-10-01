538ed7f4b24a83056e5fbc450452c306c676f0e5
25-Ayush <155434945+25-Ayush@users.noreply.github.com>
2025-10-01 06:31:49 +0000

    operations

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -1,9 +1,9 @@
 let score = "25ab";
-console.log(typeof score); //string
+//console.log(typeof score); //string
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber); //number
-console.log(valueInNumber); 
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); 
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn); //true
+//console.log(booleanIsLoggedIn); //true
 
 // 1 => true ; 0 => false
 // "" =>false
@@ -23,6 +23,52 @@ console.log(booleanIsLoggedIn); //true
 let someNumber = 25;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ***************** Operations ***************************
+
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = 'Ayush'
+let str2 = ' Phapale'
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log('1' + 2); 
+// console.log(1 + '2'); 
+// console.log('1'+2+2); 
+// console.log(1+2+'2');
+
+//If at least one operand is a string → string concatenation
+//(everything gets converted to string from that point onward)
+
+
+let counter = 100;
+counter++;
+++counter;
+console.log(counter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
