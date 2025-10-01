20f96f1ce2a0e0bca812a6ee1e9dcd28c30f7ad2
25-Ayush <155434945+25-Ayush@users.noreply.github.com>
2025-10-01 07:50:37 +0000

    comparision

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+
+// console.log('2'>1);
+// console.log('02'>1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+//Relational operators (>, >=, <, <=) → convert null into a number (0).
+
+// Equality operator (==) → treats null as equal only to undefined, not to numbers.
+
+
+// console.log(undefined > 0);
+// console.log(undefined ==0);
+// console.log(undefined < 0);
+
+
+// ===
+console.log('2'===2); 
+
+
+
+
+
+
