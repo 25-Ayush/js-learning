895e97ad1f0050455fbce45901179631e51cdc95
25-Ayush <155434945+25-Ayush@users.noreply.github.com>
2025-11-16 13:25:47 +0000

    closure and getters and setters

---
+++ 10_classes_oops/Object_get_set.js
@@ -0,0 +1,17 @@
+const User = {
+    _email : 'abc@xyz.com',
+    _password : 'abc',
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    },
+
+
+}
+
+const userOne = Object.create(User)
+console.log(userOne.email);
